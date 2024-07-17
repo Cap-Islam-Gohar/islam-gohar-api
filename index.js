fed73db4b18a97af75d0917af68e1bc654af8588
Cap-Islam-Gohar <cap.islamadel@gmail.com>
2024-07-17 05:14:33 +0300

    test json data

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 
 
-let transactions = async () => {
-    let response = await fetch("http://localhost:3000/transactions");
+let customers = async () => {
+    let response = await fetch("http://localhost:3000/customers");
     return await response.json();
 }
 
-transactions().then((data) => {
-    console.log(data)
+customers().then((data) => {
+
+    const container = document.createElement("div");
+
+    data.forEach((customer) => {
+        const row = document.createElement("div");
+        row.innerHTML = customer.name;
+        container.appendChild(row)
+    });
+
+    document.body.appendChild(container);
+    
+
+
 });
